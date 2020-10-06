9cbc0f58d7577d229cd59a7928ec7a5dcbe4229a
brandonvietlam <56048840+brandonvietlam@users.noreply.github.com>
2020-10-06 16:59:10 -0500

    Update Brandon Lam EC1.py
    add dynamic programming

--- Jerry/Brandon Lam EC1.py
+++ Jerry/Brandon Lam EC1.py
@@ -2,14 +2,22 @@
 
 # Recursion: Davis' Staircase
 # https://www.hackerrank.com/challenges/ctci-recursive-staircase/problem
-def stepPerms(n):
-    # this solution works but it times out :/
-    if(n<0):
-        return 0
-    if(n==0):
-        return 1
+# new solution uses dynamic programming!
+# thanks jerry!
+def stepPerms(n, memo={0: 1}):
+    if(n<=0):
+        if(n<0):
+          return 0
+        else:
+          return 1
+    elif n in memo.keys():
+        return memo[n]
     else:
-        return stepPerms(n-1) + stepPerms(n-2) + stepPerms(n-3)
+        memo[n-3] = stepPerms(n-3, memo)
+        memo[n-2] = stepPerms(n-2, memo)
+        memo[n-1] = stepPerms(n-1, memo)
+        memo[n] = memo[n-3] + memo[n-2] + memo[n-1]
+        return memo[n]
 
 s = int(input())
 for s_itr in range(s):
